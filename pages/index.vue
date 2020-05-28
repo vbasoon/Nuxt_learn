cc5c6cdba928a9370586166f0f54f0fe2b308168
Vlad Basoon <vbasoon@gmail.com>
2020-05-28 11:37:25 +0300

    створив гілку Накст

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <b-col cols="8">Hello</b-col>
       <b-col cols="4">Nuxt!</b-col>
     </b-row>
-    Counter: {{ $store.state.counter }}
-    <b-btn @click="$store.commit('increment')">Збільшити </b-btn>
+    Counter: {{ $store.state.counter.counter }}
+    <b-btn @click="$store.commit('counter/increment')">Збільшити </b-btn>
   </b-container>
 </template>
