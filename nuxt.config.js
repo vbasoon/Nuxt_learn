bc86fe859d9d3a31210ce4e38f07fae1ed1b7c1f
Vlad Basoon <vbasoon@gmail.com>
2020-05-28 10:56:38 +0300

    add bootstrap modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,7 @@ module.exports = {
   /*
   ** Build configuration
   */
+  modules: ['bootstrap-vue/nuxt'],
   build: {
     /*
     ** Run ESLint on save
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-      <div>
-        <h1>Events</h1>
-      </div>
-    </template>
+<b-container>
+  <b-row class="w-100">
+    <b-col cols="8">Hello</b-col>
+    <b-col cols="4">Nuxt!</b-col>
+  </b-row>
+</b-container>
+</template>
