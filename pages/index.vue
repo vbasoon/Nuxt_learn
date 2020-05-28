9181ee751eb8f73407d1d67f733646ae74fea3a7
Vlad Basoon <vbasoon@gmail.com>
2020-05-28 11:09:59 +0300

    creating counter

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
-<b-container>
-  <b-row class="w-100">
-    <b-col cols="8">Hello</b-col>
-    <b-col cols="4">Nuxt!</b-col>
-  </b-row>
-</b-container>
+  <b-container>
+    <b-row class="w-100">
+      <b-col cols="8">Hello</b-col>
+      <b-col cols="4">Nuxt!</b-col>
+    </b-row>
+    Counter: {{ $store.state.counter }}
+    <b-btn @click="$store.commit('increment')">Збільшити </b-btn>
+  </b-container>
 </template>
